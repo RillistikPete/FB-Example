523731225c4df9303cb42bf5bb454b4ef7f9be3f
Peter Forrest <petergforrest91@gmail.com>
2018-01-04 17:14:24 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FizzBuzz C#");
+            foreach (var number in Enumerable.Range(1, 100))
+            {
+                string output = null;
+                if(number % 3 == 0) { output = number + ": Fizz";}
+                if(number % 5 == 0) { output = number + ": Buzz";}
+                if(number % 15 == 0) { output = number + ": FizzBuzz";}
+                output = (output == null) ? number.ToString() : output;
+                Console.WriteLine(output);
+
+            }
+
+
+        }
+    }
+}
